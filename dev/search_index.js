db614bd20361b2295a71e02c2dd9e168b6c650a1
Documenter.jl <documenter@juliadocs.github.io>
2025-12-03 18:01:14 +0000

    build based on 15e495a

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#Ishtar","page":"Home","title":"Ishtar","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Ishtar.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+[{"location":"#Ishtar","page":"Home","title":"Ishtar","text":"Documentation for Ishtar.\n\n","category":"section"}]
 }
